b2491c1e82b53adf332d54c470dcc0424deac7a5
krishgn <krish.kgn@gmail.com>
2020-04-21 17:53:45 -0700

    hangman game

--- 3.Hangman.py
+++ 3.Hangman.py
@@ -1,10 +1,18 @@
+'''
+Here I implement the game of Hangman. The words that can be tested are initialized in a list as global variable.
+This can be modified by the user.
+'''
+
 from random import randint
 wordlist = ["insight","artificial","intelligence","machine","learning", \
-            "silicon","management","program"]
+            "silicon","management","program",'clustering', 'regression', 'likelihood']
 listlen = len(wordlist)
 
 class Word:
     def __init__(self):
+        '''
+        Initializing the word class
+        '''
         val = randint(0, listlen-1)
         self.word = wordlist[val]
         self.charlist = list(self.word)
@@ -13,7 +21,6 @@ class Word:
         print(self.anslist)
         self.wrongguess = 0
         self.wronglist = []
-        self.image = Picture()
 
     def checkchar(self,char):
         if char in self.charlist:
@@ -30,34 +37,58 @@ class Word:
                 self.wrongguess +=1
                 self.wronglist.append(char)
                 print("You have " + str(6-self.wrongguess) + " guesses remainig")
-            self.image.display(self.wrongguess)
+            self.display(self.wrongguess)
         return self.wrongguess, self.anslist
-
-class Picture:
-    def __init__self():
-        self.symbols = ['0','|','/','\\','/','\\']
 
     def display(self,wrongguess):
         if wrongguess == 1:
-            print('0')
+            print("_________")
+            print("|     |")
+            print("|	 O")
+            print("|")
+            print("|")
+            print("|")
+            print("|________")
         elif wrongguess == 2:
-            print(' 0')
-            print(' |')
+            print("_________")
+            print("|	 |")
+            print("|	 O")
+            print("|	 |")
+            print("|	 |")
+            print("|")
+            print("|________")
         elif wrongguess == 3:
-            print(' 0')
-            print('/|')
+            print("_________")
+            print("|	 |")
+            print("|	 O")
+            print("|	\|")
+            print("|	 |")
+            print("|")
+            print("|________")
         elif wrongguess == 4:
-            print(' 0')
-            print('/|\\')
-        elif wrongguess == 4:
-            print(' 0')
-            print('/|\\')
-            print('/')
+            print("_________")
+            print("|	 |")
+            print("|	 O")
+            print("|	\|/")
+            print("|	 |")
+            print("|")
+            print("|________")
         elif wrongguess == 5:
-            print(' 0')
-            print('/|\\')
-            print('/\\')
-
+            print("_________")
+            print("|	 |")
+            print("|	 O")
+            print("|	\|/")
+            print("|	 |")
+            print("|	/ ")
+            print("|________")
+        elif wrongguess == 6:
+            print("_________")
+            print("|	 |")
+            print("|	 O")
+            print("|	\|/")
+            print("|	 |")
+            print("|	/ \ ")
+            print("|________")
 
 class Hangman:
     def __init__(self):
@@ -84,8 +115,6 @@ class Hangman:
             print("Congratulations!")
             return True
         return False
-
-
 
 if __name__ == "__main__":
     game = Hangman()
